691619e87b6c2be3368383aa41da64aff2e1e79c
chess2022 <lakeshore.hnh@gmail.com>
2022-03-21 17:47:45 -0700

    Initiates initial file structure

---
+++ js/script.js
@@ -0,0 +1,49 @@
+/*
+LIST OF ALL INGREDIENTS
+const settings = {
+  async: true,
+  crossDomain: true,
+  url: "https://the-cocktail-db.p.rapidapi.com/list.php?i=list",
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+    "x-rapidapi-key": "bcea58ebc5mshb2a5ad3f780d9dep1a9ff0jsn6456bc6fdba7",
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+*/
+
+// FILTER BY MULTI-INGREDIENT
+// const settings = {
+//   async: true,
+//   crossDomain: true,
+//   url: "https://the-cocktail-db.p.rapidapi.com/filter.php?i=Dry_Vermouth%2CGin%2CAnis",
+//   method: "GET",
+//   headers: {
+//     "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+//     "x-rapidapi-key": "bcea58ebc5mshb2a5ad3f780d9dep1a9ff0jsn6456bc6fdba7",
+//   },
+// };
+
+// $.ajax(settings).done(function (response) {
+//   console.log(response);
+// });
+
+// SEARCH BY INGREDIENT
+// const settings = {
+//   async: true,
+//   crossDomain: true,
+//   url: "https://the-cocktail-db.p.rapidapi.com/filter.php?i=Gin",
+//   method: "GET",
+//   headers: {
+//     "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+//     "x-rapidapi-key": "bcea58ebc5mshb2a5ad3f780d9dep1a9ff0jsn6456bc6fdba7",
+//   },
+// };
+
+// $.ajax(settings).done(function (response) {
+//   console.log(response);
+// });
