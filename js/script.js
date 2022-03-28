0e7101ff31c03ef4d47aae2cafc391d1e22b7511
chess2022 <lakeshore.hnh@gmail.com>
2022-03-28 12:07:11 -0700

    adds code for recipe api

--- js/script.js
+++ js/script.js
@@ -52,8 +52,10 @@ const APIKEY = "9973533";
 // put click event on the button
 
 $button.on("click", () => {
+  
   // get the text the user types
   let searchTerm = $input.val();
+
 
   $.ajax(`https://www.thecocktaildb.com/api/json/v2/1/filter.php?i=${searchTerm}&appid=${APIKEY}`
       ).then((data) => {
@@ -66,18 +68,75 @@ $button.on("click", () => {
           container.append(cell);
           img.src = `${data.drinks[i].strDrinkThumb}`;
           title.textContent = `${data.drinks[i].strDrink}`;
-          cell.append(img, title)
-          console.log(data)
+          cell.id = `${data.drinks[i].idDrink}`;
+          // cell.innerHTML = `<a class="btn" class="box" onclick="pop()"></a>`
+          cell.append(img, title);
+
+          const $drink = $(cell);
+
+          $drink.on("click", () => {
+            $.ajax(
+              `https://www.thecocktaildb.com/api/json/v2/1/lookup.php?i=${cell.id}&appid=${APIKEY}`
+            ).then((recipe) => {
+
+              console.log(recipe)
+              // make grid for the results to populate in
+              // for (let i = 0; i < recipe.drinks.length; i++) {
+              const recipeContainer = document.getElementById("recipe");
+              let bigImg = document.createElement("img");
+              let recipeTitle = document.createElement("h3");
+              let recipeCell = document.createElement("div");
+              let recipeCell2 = document.createElement("div");
+              recipeContainer.append(recipeCell, recipeCell2);
+              bigImg.src = `${recipe.drinks[0].strDrinkThumb}`;
+              recipeTitle.textContent = `${recipe.drinks[0].strDrink}`;
+              recipeCell.append(bigImg, recipeTitle);
+              let ingredients = document.createElement("ul");
+              let instructions = document.createElement("p");
+              ingredients.li = `${recipe.drinks[0].strIngredient1} + ${recipe.drinks[0].strMeasure1}`;
+              instructions.textContent = `${recipe.drinks[0].strInstructions}`;
+              recipeCell2.append(ingredients, p);
+            });
+          });
         }
- 
-          
-        
+})
+
+})
 
 
-        console.log(data);
-        console.log(data.drinks)
-      })
-});
+ // www.thecocktaildb.com/api/json/v2/1/lookup.php?i=${cell.id}&appid=${APIKEY}
+
+
 
 
 // !!NEED TO: clear search box after button is pushed & clear results from last search when new search is initiated
+
+        // testing out floating window
+
+          // cell.innerHTML = `<a class="btn" class="box" onclick="pop()"></a>`
+
+
+        // var modal = null;
+        // function pop() {
+        //   if (modal === null) {
+        //     document.container.cell.style.display = "block";
+        //     modal = true;
+        //   } else {
+        //     document.container.cell.style.display = "none";
+        //     modal = null;
+        //   }
+        // }
+
+        // another method to test:
+
+        // $(function () {
+        //   $(document.container.img).on("click", function () {
+        //     $("#recipe-panel").addClass("PanelFloat");
+        //   });
+        //   $(document.container.cell).on("click", function () {
+        //     $("#recipe-panel").removeClass("PanelFloat");
+        //   });
+
+        // console.log(data);
+        // console.log(data.drinks);
+        // });
